d52c3b9cd50163ab0eb66fb654d87907aa139c1b
Siddique Data Engineer <dba.siddique@gmail.com>
2015-08-06 09:59:00 +0500

    Implement data processing pipeline

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+72 Predictive Maintenance - Main Implementation
+"""
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("Starting 72-predictive-maintenance")
+    # Implementation here
+    pass
+
+if __name__ == "__main__":
+    main()
